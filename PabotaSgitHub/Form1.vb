11706b15bc9ff1c27150ed0b63152ad5b5c8d2ac
AkronimS <115407192+AkronimS@users.noreply.github.com>
2022-10-09 18:39:56 +0300

    Update Form1.vb

--- PabotaSgitHub/Form1.vb
+++ PabotaSgitHub/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        TextBox1.Text = "Connect"
+        TextBox1.Text = "Connect 12"
     End Sub
 End Class
