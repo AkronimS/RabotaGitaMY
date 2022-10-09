adba7d474aac292a9bd5b90e8fb70e79a080ffc6
AkronimS <115407192+AkronimS@users.noreply.github.com>
2022-10-09 18:49:17 +0300

    Дополнение

--- PabotaSgitHub/Form1.vb
+++ PabotaSgitHub/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         TextBox1.Text = "Connect 12"
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("Connect")
+    End Sub
 End Class
